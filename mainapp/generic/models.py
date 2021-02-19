5402f2ac8408ef7e84b08d46d9382c0f7aa554d6
anasir514 <anasir514@gmail.com>
2021-02-19 14:07:14 +0000

    c

--- mainapp/generic/models.py
+++ mainapp/generic/models.py
@@ -4,7 +4,7 @@ from wagtail.core.models import Page
 from wagtail.admin.edit_handlers import FieldPanel
 
 class genericPage(Page):
-    banner_title = models.CharField(max_length=100, default='welcome to my genericPage')
+    banner_title = models.CharField(max_length=100, default='welcome to my homepage')
 
     content_panels = Page.content_panels + [
         FieldPanel("banner_title"),
